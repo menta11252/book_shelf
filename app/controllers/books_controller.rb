28f2b4a9553365f8d234e61ed0e5104e53fc584e
menta11252 <dyky.1012@gmail.com>
2019-05-03 19:27:29 +0900

    バリデーション実行

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,23 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
-        end      
-        def create
-        @book = Book.new(book_params)
-          @book.save
-        redirect_to @book, notice: "書籍を登録しました。"
-      end      
-      private      
-      def book_params
-        params.require(:book).permit(:title, :price, :publish_date, :description)
+  end      
+        
+  def create
+      @book = Book.new(book_params)
+      if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+      else
+        render :new
       end
+  end      
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private      
+    def book_params
+      params.require(:book).permit(:title, :price, :publish_date, :description)
+    end
 end
