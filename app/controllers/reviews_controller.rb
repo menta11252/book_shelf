67d20b71ff8fb29816cdd2d3fa403ffaf47eeb3c
menta11252 <dyky.1012@gmail.com>
2019-05-04 17:45:55 +0900

    レビュー投稿機能まで

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
 
-  before_action :set_book, only: [:new, :create]
+  before_action :set_book, only: [:show, :new, :create, :edit]
+  before_action :set_review, only: [:show, :edit, :update]
   def new
     @review = Review.new
   end 
@@ -16,11 +17,27 @@ class ReviewsController < ApplicationController
       render :new
     end
   end 
+    def show
+    end
+
+    def edit
+    end 
+  def update
+    if @review.update(review_params)
+      redirect_to @review.book, notice: "レビューを更新しました。"
+    else
+      render :edit
+    end
+  end
   private 
   def review_params
     params.require(:review).permit(:title, :body, :evaluation)
   end 
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 end
